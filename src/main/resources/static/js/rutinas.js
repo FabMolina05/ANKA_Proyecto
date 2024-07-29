02d5dad6c1443ffc4f10086c836625eea8e60cf4
dan <dan@aira>
2024-07-28 20:57:55 -0600

    Proyecto ANKA

--- src/main/resources/static/js/rutinas.js
+++ src/main/resources/static/js/rutinas.js
@@ -1,16 +1,26 @@
 /*
- * Esta funcion muestra una imagen seleccionada en la pantalla
+ * Esta funcion muestra los datos de la tarjeta seleccionada en un Modal
  */
 
-function readURL(input){
-    if (input.files && input.files[0]) {
-        var reader = new FileReader;
-        reader.onload=function(e){
-            $('#blah').attr('src',e.target.result)
-                    .height(200);
-        };
-        reader.readAsDataURL(input.files[0]);
+document.onreadystatechange = function () {
+    if (document.readyState == "interactive") {
+        const exampleModal = document.getElementById('exampleModal');
+        if (exampleModal) {
+            exampleModal.addEventListener('show.bs.modal', event => {
+                const button = event.relatedTarget;
+                const nombre = button.getAttribute('data-nombre');
+                const imagen = button.getAttribute('data-imagen');
+                const descripcion = button.getAttribute('data-descripcion');
+                
+                const modalTitle = exampleModal.querySelector('.modal-title');
+//                const modalBodyInput = exampleModal.querySelector('.modal-body p')
+                const modalImage = exampleModal.querySelector('#modal-image');
+                const modalDescripcion = exampleModal.querySelector('#modal-body-descripcion');
+                
+                modalTitle.textContent = nombre;
+                modalImage.src = imagen;
+                modalDescripcion.textContent = descripcion;
+            })
+        }
     }
 }
-
-
